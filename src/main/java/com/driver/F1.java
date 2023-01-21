2e0dbc24c13e31750e5d859eb8cfe8b61b0d1b64
Mohammad-Mubarak <beyourbestversion3@gmail.com>
2023-01-21 21:21:16 +0530

    done hogya

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,30 +1,44 @@
 package com.driver;
 
-public class F1 extends Car {
+public class Vehicle {
 
-    public F1(String name, boolean isManual) {
-        //Use arbitrary values for parameters which are not mentioned
+    private String name;
+    private int currentSpeed;
+    private int currentDirection;
+
+    public Vehicle(String name) {
+        this.name = name;
+        this.currentSpeed = 0;
+        this.currentDirection = 0;
     }
 
-    public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        /**
-         * speed 0: gear 1
-         * speed 1-50: gear 1
-         * speed 51-100: gear 2
-         * speed 101-150: gear 3
-         * speed 151-200: gear 4
-         * speed 201-250: gear 5
-         * speed more than 250: gear 6
-         */
+    public void steer(int direction){
+        //direction is in degrees, add it to the current direction
+        this.currentDirection+=direction;
+        System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
+    }
 
-        if(newSpeed == 0) {
-            //Stop the car, set gear as 1
-        }
-        //for all other cases, change the gear accordingly
+    public void move(int speed, int direction){
+        //set the values of currentSpeed and currentDirection
+        this.currentSpeed = speed;
+        this.currentDirection = direction;
+        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
+    }
 
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
-        }
+    public void stop(){
+        this.currentSpeed = 0;
+        System.out.println("stop method called - The vehicle is stopped");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
     }
 }
